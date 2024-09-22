4bc9815e524133b2031036023393b22e1832b048
Friden <fridenzhang@gmail.com>
2024-09-22 09:20:15 +0800

    fix: fix typo

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn panic(_info: &PanicInfo) -> ! {
 /*
 By using the #[no_mangle] attribute, we disable name mangling to ensure that the Rust
 compiler really outputs a function with the name _start. Without the attribute, the
-compiler would generate some cryptic _ZN3blog_os4_start7hb173fedf945531caE symbol to
+compiler would generate some cryptic _ZN3rust_op_system4_start7hb173fedf945531caE symbol to
 give every function a unique name. The attribute is required because we need to tell
 the name of the entry point function to the linker in the next step.
 */
